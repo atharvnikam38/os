80b9fd2d8f3357291e8c49796328bb97c2673c57
w4lyf <159780645+w4lyf@users.noreply.github.com>
2024-04-04 03:14:44 +0530

    Update parr.pl

--- parr.pl
+++ parr.pl
@@ -3,28 +3,33 @@
 use strict;
 use warnings;
 
-sub print_elements_in_range {
-    my ($range_start, $range_end, @array) = @_;
+my @array;
 
-    foreach my $element (@array) {
-        if ($element >= $range_start && $element <= $range_end) {
-            print "$element ";
-        }
-    }
-    print "\n";
+print "Enter the size of the array: ";
+my $size = <STDIN>;
+chomp $size;
+
+for (my $i = 0; $i < $size; $i++) {
+    print "Enter element $i: ";
+    my $element_input = <STDIN>;  
+    chomp $element_input;
+    push @array, $element_input;
 }
 
-print "Enter the start of the range: ";
-my $start = <STDIN>;
-chomp $start;
+print "Enter the lower bound of the range: ";
+my $lower_bound_input = <STDIN>; 
+chomp $lower_bound_input;
+my $lower_bound = int($lower_bound_input);  
 
-print "Enter the end of the range: ";
-my $end = <STDIN>;
-chomp $end;
+print "Enter the upper bound of the range: ";
+my $upper_bound_input = <STDIN>;   
+chomp $upper_bound_input;
+my $upper_bound = int($upper_bound_input); 
 
-print "Enter the array elements separated by space: ";
-my $input = <STDIN>;
-chomp $input;
-my @array = split(' ', $input);
-
-print_elements_in_range($start, $end, @array);
+print "Elements within the range $lower_bound - $upper_bound: ";
+foreach my $element (@array) {
+    if ($element >= $lower_bound && $element <= $upper_bound) {
+        print "$element ";
+    }
+}
+print "\n";
