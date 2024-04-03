9ac70bc127e041da2b87ef0145e009991c628ede
w4lyf <159780645+w4lyf@users.noreply.github.com>
2024-04-04 03:07:32 +0530

    Update pmerge.pl

--- pmerge.pl
+++ pmerge.pl
@@ -1,9 +1,37 @@
 #!/usr/bin/perl
 
-my @array1 = (1, 2, 3);
-my @array2 = (4, 5, 6);
-my @array3 = (7, 8, 9);
+use strict;
+use warnings;
 
-my @merged_array = (@array1, @array2, @array3);
+my @array1;
+my @array2;
 
-print "Merged array: @merged_array\n";
+print "Enter the size of the first array: ";
+my $size1 = <STDIN>;
+chomp $size1;
+
+for (my $i = 0; $i < $size1; $i++) {
+    print "Enter element $i of the first array: ";
+    my $element = <STDIN>;
+    chomp $element;
+    push @array1, $element;
+}
+
+print "Enter the size of the second array: ";
+my $size2 = <STDIN>;
+chomp $size2;
+
+for (my $i = 0; $i < $size2; $i++) {
+    print "Enter element $i of the second array: ";
+    my $element = <STDIN>;
+    chomp $element;
+    push @array2, $element;
+}
+
+my @merged_array = (@array1, @array2);
+
+print "Merged array: ";
+foreach my $element (@merged_array) {
+    print "$element ";
+}
+print "\n";
