5f1fd0c0838588c4c45b0906470156506a2bf14e
w4lyf <159780645+w4lyf@users.noreply.github.com>
2024-04-04 03:04:46 +0530

    Update psort.pl

--- psort.pl
+++ psort.pl
@@ -1,5 +1,25 @@
 #!/usr/bin/perl
 
-my @array = (5, 2, 9, 1, 3);
-my @sorted_array = sort @array;
-print "Sorted array: @sorted_array\n";
+use strict;
+use warnings;
+
+print "Enter the size of the array: ";
+my $size = <STDIN>;
+chomp $size;
+
+my @array;
+
+for (my $i = 0; $i < $size; $i++) {
+    print "Enter element $i: ";
+    my $element = <STDIN>;
+    chomp $element;
+    push @array, $element;
+}
+
+@array = sort { $a <=> $b } @array;
+
+print "Sorted array: ";
+foreach my $element (@array) {
+    print "$element ";
+}
+print "\n";
