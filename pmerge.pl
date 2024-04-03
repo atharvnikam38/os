f7e3951be18bfa7939d6298b1ea60b5ea8b14b25
w4lyf <159780645+w4lyf@users.noreply.github.com>
2024-04-04 02:13:51 +0530

    Create pmerge.pl

---
+++ pmerge.pl
@@ -0,0 +1,9 @@
+#!/usr/bin/perl
+
+my @array1 = (1, 2, 3);
+my @array2 = (4, 5, 6);
+my @array3 = (7, 8, 9);
+
+my @merged_array = (@array1, @array2, @array3);
+
+print "Merged array: @merged_array\n";
